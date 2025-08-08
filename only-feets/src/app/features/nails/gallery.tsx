7cc01f3ba23c3a137ec30c317e2589ac338c00e3
diegoarpar <diegoarpar@gmail.com>
2025-08-08 19:07:53 -0400

    feat(): new sections

--- only-feets/src/app/features/nails/gallery.tsx
+++ only-feets/src/app/features/nails/gallery.tsx
@@ -5,7 +5,7 @@ import Gallery from '../../components/gallery/gallery';
 export default function GalleryFeature() {
   const imageList = []; // Initialize an empty array
   for (let i = 1; i <= 21; i++) {
-    imageList.push("/images/nails/" + i + ".jpg"); // Process and add to the array
+    imageList.push("/images/nails/nails" + i + ".jpg"); // Process and add to the array
   }
   return (
     <div>
